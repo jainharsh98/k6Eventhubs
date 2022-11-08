6baab54fc02e3e91db0fc89abb76d21a232e996d
Harsh Jain <jainh@microsoft.com>
2022-11-08 14:25:11 +0530

    Adding Eventhub load test script

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check } from 'k6';
+
+let ehSasToken = __ENV.EVENTHUB_ACCESS_TOKEN;
+let ehNamespace = __ENV.EVENTHUB_NAMESPACE;
+let ehName = __ENV.EVENTHUB_NAME;
 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
-}
+    const body = {
+        key: "randomKey1",
+        value: "randomKey2",
+    };
+    var params = {
+        headers: {
+            'Content-Type': 'application/atom+xml;type=entry;charset=utf-8',
+            'Authorization': `SharedAccessSignature ${ehSasToken}`,
+            'Host': `${ehNamespace}.servicebus.windows.net`
+        },
+    };
+
+    var uri = `https://${ehNamespace}.servicebus.windows.net/${ehName}/messages`
+    var url = `${uri}?api-version=2014-01`
+    let res = http.post(url, JSON.stringify(body), params);
+
+    check(res, {
+        "is status 200": r => r.status >= 200 && r.status <= 300
+    });
+
+};
+
